bb64d12923c4cae48030e15c12dc79640d1f66a0
137799 <g1377@protonmail.com>
2020-08-18 00:17:42 +0300

    - psvm

--- Tasks4j/src/main/ru.j4j/Psvm.java
+++
@@ -1,8 +0,0 @@
-
-import data.TaskDB;
-
-public class Psvm {
-    public static void main(String[] args) {
-        TaskDB.getInstance().findAllForUserId(18).stream().forEach(System.out::println);
-    }
-}
